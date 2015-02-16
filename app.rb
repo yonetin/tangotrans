8facdc3f5a3275bbb5eca99ca5cb010fd19326c6
yonetin <yonekou1110@gmail.com>
2015-02-16 18:38:09 +0900

    edit app.rb

--- app.rb
+++ app.rb
@@ -18,8 +18,8 @@ require 'nokogiri'
 # heroku postgresqlの接続用コード
 ActiveRecord::Base.establish_connection(ENV['postgres://sdezcgxojqsddd:tMzk-k8yYm_vKQ2YTxYODK90lD@ec2-107-21-93-97.compute-1.amazonaws.com:5432/d8esrjg5rn2qde'])
 
-use ActiveRecord::ConnectionAdapters::ConnectionManagement
-run Sinatra::Application
+#use ActiveRecord::ConnectionAdapters::ConnectionManagement
+#run Sinatra::Application
 
 class Tango < ActiveRecord::Base
 end
